c6023f04bf11ca83bbe19cc555c132dbce0d0b0d
Kobi Hari <kobi2294@yahoo.com>
2022-02-09 16:25:18 +0200

    Added module 02 folder

---
+++ Module 02 - Rx Factories/IntroToRx/IntroToRx/ConsoleObserver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToRx
+{
+    public class ConsoleObserver<T> : IObserver<T>
+    {
+        private string _id;
+        private ConsoleColor _color;
+        private static object _lock = new object();
+
+        public ConsoleObserver(string id, ConsoleColor color)
+        {
+            _id = id;
+            _color = color;
+        }
+
+        private void Print(string txt)
+        {
+            lock(_lock)
+            {
+                var oldColor = Console.ForegroundColor;
+                Console.ForegroundColor = _color;
+                Console.WriteLine(txt);
+                Console.ForegroundColor = oldColor;
+            }
+        }
+
+        public void OnCompleted()
+        {
+            Print($"{DateTime.Now} Observer {_id} Completed");
+        }
+
+        public void OnError(Exception error)
+        {
+            Print($"{DateTime.Now} Observer {_id} Error {error.Message}");
+        }
+
+        public void OnNext(T value)
+        {
+            Print($"{DateTime.Now} Observer {_id} Next {value}");
+        }
+    }
+}
---
+++ Module 02 - Rx Factories/IntroToRx/IntroToRx/MyCustomObservable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Disposables;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToRx
+{
+    public class MyCustomObservable : IObservable<int>
+    {
+        private async void RunObserverSequence(IObserver<int> observer)
+        {
+            observer.OnNext(42);
+
+            await Task.Delay(2000);
+
+            observer.OnNext(100);
+
+            await Task.Delay(2000);
+
+            observer.OnCompleted();
+
+            await Task.Delay(1000);
+            observer.OnNext(-10);
+            observer.OnError(new InvalidOperationException("This can not be"));
+        }
+
+        public IDisposable Subscribe(IObserver<int> observer)
+        {
+            RunObserverSequence(observer);
+
+            return Disposable.Empty;
+        }
+    }
+}
