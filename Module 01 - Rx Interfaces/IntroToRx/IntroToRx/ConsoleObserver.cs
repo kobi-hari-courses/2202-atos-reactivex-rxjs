b51ce6583a712bcf6c71da29ff6d0ba0053f2f4f
Kobi Hari <kobi2294@yahoo.com>
2022-02-09 16:52:37 +0200

    Restored module 01 status

--- Module 01 - Rx Interfaces/IntroToRx/IntroToRx/ConsoleObserver.cs
+++ Module 01 - Rx Interfaces/IntroToRx/IntroToRx/ConsoleObserver.cs
@@ -9,39 +9,25 @@ namespace IntroToRx
     public class ConsoleObserver<T> : IObserver<T>
     {
         private string _id;
-        private ConsoleColor _color;
-        private static object _lock = new object();
 
-        public ConsoleObserver(string id, ConsoleColor color)
+        public ConsoleObserver(string id)
         {
             _id = id;
-            _color = color;
-        }
-
-        private void Print(string txt)
-        {
-            lock(_lock)
-            {
-                var oldColor = Console.ForegroundColor;
-                Console.ForegroundColor = _color;
-                Console.WriteLine(txt);
-                Console.ForegroundColor = oldColor;
-            }
         }
 
         public void OnCompleted()
         {
-            Print($"{DateTime.Now} Observer {_id} Completed");
+            Console.WriteLine($"{DateTime.Now} Observer {_id} Completed");
         }
 
         public void OnError(Exception error)
         {
-            Print($"{DateTime.Now} Observer {_id} Error {error.Message}");
+            Console.WriteLine($"{DateTime.Now} Observer {_id} Error {error.Message}");
         }
 
         public void OnNext(T value)
         {
-            Print($"{DateTime.Now} Observer {_id} Next {value}");
+            Console.WriteLine($"{DateTime.Now} Observer {_id} Next {value}");
         }
     }
 }
